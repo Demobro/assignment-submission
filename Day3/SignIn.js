d31757e288b1cdbab7181289dc49fbcc4547c912
Akash Verma <95988590+akku8112@users.noreply.github.com>
2022-01-29 16:11:14 +0530

    Day3 Assignment sumitted

---
+++ Day3/SignIn.js
@@ -0,0 +1,11 @@
+
+
+const email = prompt('What is your email address ?');
+const password = prompt('What is your password ?');
+
+if (email === 'Student123@gamil.com' && password === '1234'){
+    alert('welcome!');
+}
+else{
+    alert('Access Denied !');
+}
